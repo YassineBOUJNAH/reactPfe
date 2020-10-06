01d701446f2f3f137e02926a444e0f42ce518626
llyasstrh <ilyasstrh@gmail.com>
2020-10-06 01:52:58 +0100

    update route students

--- src/routesStudent.js
+++ src/routesStudent.js
@@ -5,30 +5,46 @@ import Typography from "views/Typography.js";
 import TableList from "views/Tables.js";
 import Maps from "views/Map.js";
 import UserPage from "views/User.js";
+import InternshipOffers from "views/studentViews/InternshipOffers.js"
+import StudentHome from "views/studentViews/StudentHome.js"
+import StudentPosts from "views/studentViews/StudentPosts.js"
 import UpgradeToPro from "views/Upgrade.js";
 
-var routesStudent = [
-  {
-    path: "/typography",
-    name: "Typography",
-    icon: "nc-icon nc-caps-small",
-    component: Typography,
-    layout: "/admin",
-  },
-  {
-    path: "/typography",
-    name: "Typography",
-    icon: "nc-icon nc-caps-small",
-    component: Typography,
-    layout: "/admin",
-  },
-  {
-    pro: true,
-    path: "/upgrade",
-    name: "Upgrade to PRO",
-    icon: "nc-icon nc-spaceship",
-    component: UpgradeToPro,
-    layout: "/admin",
-  },
+var routesStudent = [{
+        path: "/home",
+        name: "Home",
+        icon: "nc-icon nc-caps-small",
+        component: StudentHome,
+        layout: "/student",
+    },
+    {
+        path: "/myoffers",
+        name: "MyOffers",
+        icon: "nc-icon nc-caps-small",
+        component: InternshipOffers,
+        layout: "/student",
+    },
+    {
+        path: "/mymeetings",
+        name: "MyMeetings",
+        icon: "nc-icon nc-caps-small",
+        component: StudentHome,
+        layout: "/student",
+    },
+    {
+        path: "/myposts",
+        name: "MyPosts",
+        icon: "nc-icon nc-caps-small",
+        component: StudentPosts,
+        layout: "/student",
+    },
+    {
+        pro: true,
+        path: "/upgrade",
+        name: "Upgrade to PRO",
+        icon: "nc-icon nc-spaceship",
+        component: UpgradeToPro,
+        layout: "/admin",
+    },
 ];
 export default routesStudent;
